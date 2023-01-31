a67465c46e8bf5b4cce138ef60a317ab5c5099cd
Sam <smarttsamy@gmail.com>
2023-01-31 12:24:59 +0530

    done

--- Array.java
+++ Array.java
@@ -1,26 +1,20 @@
-class Array {
-    static int maxSum(int arr[], int n, int k) {
-        // Initialize result
-        int max_sum = Integer.MIN_VALUE;
+import java.util.*;
 
-        // Consider all blocks starting with i.
-        for (int i = 0; i < n - k + 1; i++) {
-            int current_sum = 0;
-            for (int j = 0; j < k; j++)
-                current_sum = current_sum + arr[i + j];
-
-            // Update result if required.
-            max_sum = Math.max(current_sum, max_sum);
-        }
-
-        return max_sum;
-    }
-
-    // Driver code
+public class Array {
     public static void main(String[] args) {
-        int arr[] = { 1, 4, 2, 10, 2, 3, 1, 0, 20 };
-        int k = 4;
-        int n = arr.length;
-        System.out.println(maxSum(arr, n, k));
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] num = new int[n];
+        for (int i = 0; i <= n; i++) {
+            num[i] = in.nextInt();
+            int min = Collections.min(Arrays.asList(i));
+            int max = Collections.max(Arrays.asList(i));
+
+            int sum = min + max;
+            System.out.println("Minimum num" + min);
+            System.out.println("Maximum num" + max);
+            System.out.println(sum);
+        
+        }
     }
 }
