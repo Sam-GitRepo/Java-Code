fba9831b20373da26223771bd16d9afdf22e7b84
Sam <smarttsamy@gmail.com>
2023-06-10 19:50:31 +0530

    Sorting

---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,13 @@
+public class SelectionSort {
+    public static void main(String[] args) {
+        int [] arr = {2,1,3,64,2,11,8};
+
+        
+    }
+}
+
+/*
+ * Worst Case - O(n^2)
+ * Best Case -  O(n^2)
+ * It perform well on small lists/ arrays.
+ */
