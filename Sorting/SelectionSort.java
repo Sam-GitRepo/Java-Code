9418e293fa24ae7d38ee921d1e2caa8f135d2f28
Sam <smarttsamy@gmail.com>
2023-06-12 15:07:11 +0530

    SelectionSort

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -1,10 +1,35 @@
 public class SelectionSort {
     public static void main(String[] args) {
         int [] arr = {2,1,3,64,2,11,8};
+        selection(arr);
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
         
     }
-    static void Selection(int[] arr){
+    static void selection(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            // find the max item in the remaining array and swap with correct index
+            int last = arr.length - i - 1;
+            int maxIndex = getMaxIndex(arr, 0, last);
+            swap(arr, maxIndex, last);
+        }
+    }
 
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+    static int getMaxIndex(int[] arr, int start, int end) {
+        int max = start;
+        for (int i = start; i <= end; i++) {
+            if (arr[max] < arr[i]) {
+                max = i;
+            }
+        }
+        return max;
     }
 }
 
